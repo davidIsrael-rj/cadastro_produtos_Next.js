71e0b271bb8d768bcd808079c39ef62023cdc8e1
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-02 17:17:35 -0300

    Componente Tabela #02

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -9,34 +9,60 @@ export default function Tabela(props: TabelaProps) {
     function renderizarCabecalho() {
         return (
             <tr>
-                <th>Código</th>
-                <th>EAN</th>
-                <th>Nome</th>
-                <th>Preço Venda</th>
-                <th>Preço Custo</th>
-                <th>Margem</th>
-                <th>Alicota</th>
+                <th className="text-left p-4">Código</th>
+                <th className="text-left p-4">EAN</th>
+                <th className="text-left p-4">Nome</th>
+                <th className="text-left p-4">Preço Venda</th>
+                <th className="text-left p-4">Preço Custo</th>
+                <th className="text-left p-4">Margem</th>
+                <th className="text-left p-4">Preço Sugerido</th>
+                <th className="text-left p-4">Alicota</th>
             </tr>
         )
     }
+    function mascaraDados(dados) {
+        const resultado = (`R$ ${((dados).toFixed(2)).replace('.', ',')}`)
+        return resultado
+    }
+
+    function precoSugerido(precVenda, preCusto, margem) {
+        const precSug = (((preCusto * margem) / 100) + preCusto)
+        const resultado = `R$ ${(precSug.toFixed(2)).replace('.', ',')}`
+        if (precVenda > precSug) {
+            return <td className="text-green-600 text-center p-4">{resultado}</td>
+        } else if (precVenda === precSug) {
+            return <td className="text-blue-700 text-center p-4">{resultado}</td>
+        } else {
+            return <td className="text-red-700 text-center p-4">{resultado}</td>
+        }
+    }
+
+
     function renderizarDados() {
         return props.produtos?.map((produto, i) => {
             return (
-                <tr key={produto.id}>
-                    <td>{produto.id}</td>
-                    <td>{produto.cod_barra}</td>
-                    <td>{produto.nome}</td>
-                    <td>{produto.preco_venda}</td>
-                    <td>{produto.preco_custo}</td>
-                    <td>{produto.margem}</td>
-                    <td>{produto.alicota}</td>
+                <tr key={produto.id}
+                className={`
+                    ${i % 2 === 0 ? 'bg-green-100': 'bg-blue-100'}
+                `}>
+                    <td className="text-center p-4">{produto.id}</td>
+                    <td className="text-left p-4">{produto.cod_barra}</td>
+                    <td className="text-left p-4">{produto.nome}</td>
+                    <td className="text-center p-4">{mascaraDados(produto.preco_venda)}</td>
+                    <td className="text-center p-4">{mascaraDados(produto.preco_custo)}</td>
+                    <td className="text-center p-4">{`${produto.margem}%`}</td>
+                    {precoSugerido(produto.preco_venda, produto.preco_custo, produto.margem)}
+                    <td className="text-center p-4">{produto.alicota}</td>
                 </tr>
             )
         })
     }
     return (
-        <table>
-            <thead>
+        <table className="w-full rounded-xl overflow-hidden">
+            <thead className={`
+            text-gray-100
+                bg-gradient-to-r from-blue-600 to-green-500
+            `}>
                 {renderizarCabecalho()}
             </thead>
             <tbody>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import Produto from "@/core/Produto";
 
 export default function Home() {
   const produtos = [
-    new Produto("123456","Caneta", 1.50, 0.90, 40, "18", "1"),
-    new Produto("45","Pente", 2.50, 1.40, 40, "18", "2"),
+    new Produto("123456","Caneta", 1.19, 0.90, 40, "18", "1"),
+    new Produto("45","Pente", 1.96, 1.40, 40, "18", "2"),
     new Produto("70","Toalha", 11.50, 5.90, 40, "18", "3"),
     new Produto("55","Panela", 55.49, 20.00, 40, "18", "4"),
     new Produto("65","Camiseta", 25.90, 19.90, 40, "18", "5"),
