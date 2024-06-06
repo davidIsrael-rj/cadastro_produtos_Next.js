1087cf48d17958fe66904752e72126bac31d844f
DavidIsrael <davidisrael.rj@gmail.com>
2024-06-05 22:31:56 -0300

    Ajustando a mascara dos componentes e criando o campo preço sugerido

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,6 +1,7 @@
 import Produto from "@/core/Produto";
 import Entrada from "./Entrada";
 import { useState } from "react";
+import { mascaraDados, precoSugerido } from "@/core/funcoes";
 
 interface FormularioProps {
     produto: Produto
@@ -13,6 +14,7 @@ export default function Formulario(props: FormularioProps) {
     const [precCusto, setPrecCusto] = useState(props.produto?.preco_custo ?? '')
     const [alicota, setAlicota] = useState(props.produto?.alicota ?? '')
     const [margem, setMargem] = useState(props.produto?.margem ?? '')
+    const [pSugerido, setPSugerido]=useState(precoSugerido(precCusto, margem))
 
     return (
         <div>
@@ -27,10 +29,13 @@ export default function Formulario(props: FormularioProps) {
 
             <div className="flex justify-between items-center">
 
-                <Entrada texto="Preço Venda" valor={precVenda} classNam="flex-1" />
-                <Entrada texto="Preço Custo" valor={precCusto} classNam="flex-1" />
+                <Entrada texto="Preço Venda" valor={mascaraDados(precVenda)} classNam="flex-1" />
+                <Entrada texto="Preço Custo" valor={mascaraDados(precCusto)} classNam="flex-1" />
                 <Entrada texto="Margem de Lucro" valor={margem} classNam="flex-1" />
-                <Entrada texto="Alicota" valor={alicota} classNam="flex-1" />
+                <Entrada texto="Preço Sugerido" 
+                valor={pSugerido} 
+                classNam={`flex-1 w-2`}/>
+                <Entrada texto="Alicota" valor={alicota} classNam={`flex-1 `} />
             </div>
         </div>
     )
